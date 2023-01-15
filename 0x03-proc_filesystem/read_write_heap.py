59781adc821b8aeff10fb5ea697d7e4d8e485eb9
Achref-haddaji <haddaji.achref19@gmail.com>
2023-01-15 15:37:52 +0100

    task0

--- 0x03-proc_filesystem/read_write_heap.py
+++ 0x03-proc_filesystem/read_write_heap.py
@@ -1,43 +1,72 @@
 #!/usr/bin/python3
-import sys
-import ctypes
+""" Python Script that Manipulate ASCII
+STRING in the heap of a running PROCESS"""
+from sys import argv
 
-def find_and_replace_string(pid, search_string, replace_string):
+
+def parser():
+    """
+    Parse and return args.
+    """
+
+    pid = int(argv[1])
+    string = argv[2]
+    new = argv[3]
+    if len(new) > len(string):
+        raise IndexError
+
+    return pid, string, new
+
+
+def read_write_file(pid, string, new):
+    '''
+    Read mem file and swap the given string
+    '''
+
+    maps = "/proc/{:d}/maps".format(pid)
+    mem = "/proc/{:d}/mem".format(pid)
+    heap = "[heap]"
+    perms = {"start": None, "end": None, "perms": None}
+
+    with open(maps, "r") as maps, open(mem, "rb+") as mem:
+        for line in maps:
+            if heap in line:
+                print("[*] Found heap")
+                tmp = line.split(" ")
+                addr = tmp[0].split("-")
+                perms["start"] = int(addr[0], 16)
+                perms["end"] = int(addr[1], 16)
+                perms["perms"] = tmp[1]
+                break
+
+        if perms["perms"][:2] != "rw":
+            raise PermissionError
+
+        mem.seek(perms["start"])
+        content = mem.read(perms["end"] - perms["start"])
+        idx = content.index(bytes(string, "ASCII"))
+        print(content)
+        print("[*] Found string")
+        print("[*] Replacing string")
+        mem.seek(perms["start"] + idx)
+        mem.write(bytes(new + "\0", "ASCII"))
+
+
+def main():
+    '''
+    Entry point of the script
+    '''
     try:
-        process = psutil.Process(pid)
-    except psutil.NoSuchProcess:
-        print("Error: Invalid PID")
-        sys.exit(1)
-        
-    # Iterate through the process's memory mappings
-    for mapping in process.memory_maps():
-        # Look for mappings in the heap
-        if mapping.path.startswith('[heap]'):
-            # Open the mapping as a file
-            with open(mapping.path, 'rb+') as f:
-                # Read the data from the mapping
-                data = f.read()
-                # Replace the search string with the replace string
-                new_data = data.replace(search_string.encode(), replace_string.encode())
-                # Seek to the beginning of the file
-                f.seek(0)
-                # Write the new data to the file
-                f.write(new_data)
-                # Flush the data to disk
-                f.flush()
-                # Use msync to make sure the data is written to memory
-                addr, length = mapping.addr, mapping.length
-                libc = ctypes.CDLL('libc.so.6')
-                libc.msync(addr, length, libc.MS_SYNC)
-    print(f"Search string '{search_string}' successfully replaced with '{replace_string}'")
+        exit_value = 1
+        pid, string, new = parser()
+        read_write_file(pid, string, new)
+        exit_value = 0
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Error: Invalid number of arguments")
-        print("Usage: read_write_heap.py pid search_string replace_string")
-        sys.exit(1)
-    else:
-        pid = int(sys.argv[1])
-        search_string = sys.argv[2]
-        replace_string = sys.argv[3]
-        find_and_replace_string(pid, search_string, replace_string)
+    except Exception as e:
+        print(e)
+    finally:
+        exit(exit_value)
+
+
+if __name__ == '__main__':
+    main()
