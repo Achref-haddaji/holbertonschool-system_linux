9674f226aeeec82ac71d97a23ce1b78328d5fc63
Achref-haddaji <haddaji.achref19@gmail.com>
2023-02-09 14:47:17 +0100

    task1

--- 0x05-libasm/1-strcmp.asm
+++ 0x05-libasm/1-strcmp.asm
@@ -1,34 +1,38 @@
 BITS 64
+
 global asm_strcmp
+
 section .text
-
-asm_strcmp:
-	push rbp
-	mov rbp, rsp
-
-	mov al, [rdi]
-	mov bl, [rsi]
-	cmp al, bl
-	jne end
-
-	inc rdi
-	inc rsi
-	jmp loop
-
-loop:
-	mov al, [rdi]
-	mov bl, [rsi]
-	cmp al, bl
-	jne end
-
-	inc rdi
-	inc rsi
-	cmp al, 0
-	je end
-	jmp loop
-
-end:
-	sub al, bl
-	mov rsp, rbp
-	pop rbp
-	ret
+   asm_strcmp:
+         push rbp
+         mov rbp, rsp
+         xor rcx, rcx
+   loop_in_bytes_of_sour_and_dest:
+            mov al, [rdi + rcx]
+            mov bl, [rsi + rcx]
+            cmp al, 0
+            je compar
+            cmp bl, 0
+            je compar
+            cmp al , bl
+            jne compar
+            inc rcx
+            jmp loop_in_bytes_of_sour_and_dest
+   compar:
+      cmp al , bl
+      je equal
+      jg greater
+      jl less
+   equal:
+      mov rax , 0
+      jmp return
+   greater:
+      mov rax , 1
+      jmp return
+   less:
+      mov rax , -1
+      jmp return
+   return:
+      mov rsp , rbp
+      pop rbp
+      ret
