acf366366d479f5bf5b30bd538c9e879c4aa49de
Achref-haddaji <haddaji.achref19@gmail.com>
2023-02-20 14:22:54 +0100

    task0

---
+++ 0x06-signals/0-handle_signal.c
@@ -0,0 +1,20 @@
+#include <signal.h>
+
+void sigint_handler(int signum)
+{
+	/* signal handler function that is called when SIGINT is received */
+	printf("Gotcha! [%d]\n", signum);
+	/* prints a message indicating that the signal was caught. */
+}
+
+int handle_signal(void)
+{
+	/* function to set a handler for the SIGINT signal */
+	if (signal(SIGINT, sigint_handler) == SIG_ERR)
+	{
+		/* if an error occurred when setting the signal handler, return -1 */
+		return (-1);
+	}
+	/* if the signal handler was set successfully, return 0 */
+	return (0);
+}
