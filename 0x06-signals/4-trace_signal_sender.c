bf9ac6d29ff0867cc68c99c9a02624b6a6105d2e
Achref-haddaji <haddaji.achref19@gmail.com>
2023-02-20 15:35:07 +0100

    task4

---
+++ 0x06-signals/4-trace_signal_sender.c
@@ -0,0 +1,30 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * handler - handle the SIGQUIT signal
+ * @signum: signal number
+ */
+void handler(int signum)
+{
+    printf("SIGQUIT sent by %d\n", signum);
+}
+
+/**
+ * trace_signal_sender - trace signal sender
+ * Return: 0 on success, -1 on error
+ */
+int trace_signal_sender(void)
+{
+    struct sigaction act;
+
+    act.sa_handler = handler;
+    act.sa_flags = SA_SIGINFO;
+
+    if (sigaction(SIGQUIT, &act, NULL) < 0)
+        return -1;
+
+    return 0;
+}
