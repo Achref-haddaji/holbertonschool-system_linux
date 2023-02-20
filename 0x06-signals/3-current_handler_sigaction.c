570dff55a8b48fcd5f3ad3676fd9a63b29c568be
Achref-haddaji <haddaji.achref19@gmail.com>
2023-02-20 14:47:44 +0100

    task3

---
+++ 0x06-signals/3-current_handler_sigaction.c
@@ -0,0 +1,13 @@
+#include <signal.h>
+
+/**
+ * current_handler_sigaction - Retrieves the current handler of SIGINT
+ * Return: Pointer to signal handler function
+ */
+void (*current_handler_sigaction(void))(int)
+{
+	struct sigaction current_sig;
+
+	sigaction(SIGINT, NULL, &current_sig);
+	return (current_sig.sa_handler);
+}
