b7a0254a1a1f5ee497fecd5af6395fb4807f85e7
Achref-haddaji <haddaji.achref19@gmail.com>
2023-04-22 14:30:45 +0100

    header

---
+++ 0x09-strace/strace.h
@@ -0,0 +1,17 @@
+#ifndef _STRACE_H_
+#define _STRACE_H_
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <sys/user.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include "syscalls.h"
+#endif
